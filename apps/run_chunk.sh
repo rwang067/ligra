607f778e03f1943aa771302cbdf7ac9414a4c45b
rwang067 <wangrui1994@126.com>
2023-02-13 16:35:35 +0800

    chunk_datas+-maxiters10

--- apps/run_chunk.sh
+++ apps/run_chunk.sh
@@ -15,8 +15,16 @@ set_schedule "dynamic,64"
 declare -a rts=(26737282 18225348 5699262 254655025 310059974 233665123 35005211 691502068 739935047)
 
 # Chunk_data[0]=/mnt/nvme2/wr/friendster_chunks/out.friendster
+# Chunk_data[6]=/mnt/nvme2/wr/yahooweb/out.yahooweb
 Chunk_data[0]=/mnt/nvme2/zorax/Friendster/out.friendster
-Chunk_data[6]=/mnt/nvme2/wr/yahooweb/out.yahooweb
+Chunk_data[1]=/mnt/nvme2/zorax/Twitter/xxx
+Chunk_data[2]=/mnt/nvme2/zorax/Ukdomain/out.uk
+Chunk_data[3]=/mnt/nvme2/zorax/Kron28/out
+Chunk_data[4]=/mnt/nvme2/zorax/Kron29/out
+Chunk_data[5]=/mnt/nvme2/zorax/Kron30/out
+Chunk_data[6]=/mnt/nvme2/zorax/Yahoo/out
+Chunk_data[7]=/mnt/nvme2/zorax/Kron31/xxx
+Chunk_data[8]=/mnt/nvme2/zorax/Crawl/xxx
 
 # echo $$ | sudo tee /sys/fs/cgroup/cxy-test/cgroup.procs
 
@@ -24,8 +32,7 @@ make clean; make cleansrc;
 make testNebrs BFS PageRank Components KCore
 
 # for idx in 7
-# for idx in {0,0}
-for idx in 0
+for idx in {2,3,4,5,6}
 do
     echo "---------Chunk version-----------"
     echo -n "Data: "
@@ -55,7 +62,7 @@ do
 
     # sysctl -w vm.drop_caches=3
     echo "=======PageRank======="
-    ./PageRank -b -chunk -rounds 1 ${Chunk_data[$idx]}
+    ./PageRank -maxiters 10 -b -chunk -rounds 1 ${Chunk_data[$idx]}
     wait
 
     echo ""
@@ -68,3 +75,4 @@ done
 # set args -b -r 35005211 -m -chunk -rounds 1 /mnt/nvme2/zorax/Friendster/out.friendster
 
 # ./BFS -b -r 35005211 -m -chunk -rounds 1 /mnt/nvme2/wr/friendster_chunks/out.friendster
+# ./PageRank -maxiters 10 -b -chunk -rounds 1 /mnt/nvme2/zorax/Friendster/out.friendster
